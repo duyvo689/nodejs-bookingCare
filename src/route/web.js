1d76a2f57a339f189e18dfe3b6aff7c9a1ced016
duyvo689 <vothanhduy689@gmail.com>
2021-09-28 00:19:41 +0700

    instart sequelize + mysql

--- src/route/web.js
+++ src/route/web.js
@@ -1,12 +1,12 @@
 import express from "express"
-// import homeController from "../controllers/homeController"
+import homeController from "../controllers/homeController"
 
 let router = express.Router()
 
 let webRouters = (app) => {
-    // router.get('/', homeController.getHomePage)
+    router.get('/', homeController.getHomePage)
 
-    router.get('/', (req, res) => {
+    router.get('/docter', (req, res) => {
         return res.send('Hello world docter')
     })
 
