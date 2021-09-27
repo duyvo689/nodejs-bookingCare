7c7d9c8a4eee572314f4aa0745b91a67eccb117a
duyvo689 <vothanhduy689@gmail.com>
2021-09-27 21:45:54 +0700

    cau truc project

---
+++ src/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import bodyParser from "body-parser";
+import viewEngine from "./config/viewEngine";
+import webRouters from "./route/web";
+import connectDB from "./config/connectDB";
+require("dotenv").config();
+
+let app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+viewEngine(app);
+webRouters(app);
+
+connectDB();
+
+let port = process.env.PORT || 9000;
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
