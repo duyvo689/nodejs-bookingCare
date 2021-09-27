94fd96084955e442c2d056190fb839851d8a0e54
duyvo689 <vothanhduy689@gmail.com>
2021-09-28 00:46:22 +0700

    connectDB final

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -3,7 +3,6 @@ import db from "../models/index"
 let getHomePage = async (req, res) => {
     try {
         let data = await db.User.findAll()
-        console.log('data')
         return res.render('homepage.ejs', {
             data: JSON.stringify(data)
         })
