692904bba31dbba463d0b4066b8e99cd4850fa0e
duyvo689 <vothanhduy689@gmail.com>
2021-09-29 22:06:49 +0700

    API login

--- src/seeders/20210927170953-demo-user.js
+++ src/seeders/20210927170953-demo-user.js
@@ -3,11 +3,15 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [{
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'example@example.com',
+      email: 'duythanh@gmail.com',
+      password: 'duythanh',
+      firstName: 'Duy',
+      lastName: 'Vo',
+      address: 'Tp HCM',
+      phoneNumber: '09828394810',
+
       createdAt: new Date(),
-      updatedAt: new Date()
+      updatedAt: new Date(),
     }]);
   },
 
---
+++ src/services/userServices.js
@@ -0,0 +1,69 @@
+import db from "../models/index"
+import bcrypt from 'bcryptjs'
+import { raw } from "body-parser"
+
+let handleUserLogin = (email, password) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let userData = {}
+            let isExist = await checkUserEmail(email)
+            if (isExist) {
+                let user = await db.User.findOne({
+                    attributes: ['email', 'roleId', 'password'],
+                    where: { email: email },
+                    raw: true
+                })
+                if (user) {
+                    // so sánh password ng dùng truyền lên và từ DB (giải hash), thư viện bcryptjs làm
+                    let checkPassword = await bcrypt.compareSync(password, user.password)
+                    if (checkPassword) {
+                        userData.errCode = 0
+                        userData.errMessage = 'ok'
+
+                        delete user.password // xoá property password khi trả về người dùng
+                        userData.user = user
+                    }
+                    else {
+                        userData.errCode = 3
+                        userData.errMessage = 'Wrong password!!'
+                    }
+                }
+                else {
+                    userData.errCode = 2
+                    userData.errMessage = 'User not found!!'
+                }
+            }
+            else {
+                userData.errCode = 1
+                userData.errMessage = `Your's Email isn't exist in your system`
+            }
+            resolve(userData)
+        }
+        catch (e) {
+            reject(e)
+        }
+    })
+}
+
+let checkUserEmail = (userEmail) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let user = await db.User.findOne({
+                where: { email: userEmail }
+            })
+            if (user) {
+                resolve(true)
+            }
+            else { resolve(false) }
+        }
+        catch (e) {
+            reject(e
+            )
+        }
+    })
+}
+
+
+module.exports = {
+    handleUserLogin: handleUserLogin,
+}
