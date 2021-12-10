cc57ecc13308d1b2c6009c990ca024dc9fb19e98
superthaihoa123 <superthaihoa123@gmail.com>
2021-12-10 22:54:27 +0700

    test api get user at nodejs

--- src/route/web.js
+++ src/route/web.js
@@ -10,13 +10,7 @@ let router = express.Router()
 
 let webRouters = (app) => {
 
-    router.get('/', homeController.getHomePage)
-    router.get('/crud', homeController.getCRUD)
-    router.post('/post-crud', homeController.postCRUD)
-    router.get('/get-crud', homeController.displayGetCRUD)
-    router.get('/edit-crud', homeController.getEditCRUD)
-    router.post('/put-crud', homeController.putCRUD)
-    router.get('/delete-crud', homeController.deleteCRUD)
+
 
     // APIs for project
 
